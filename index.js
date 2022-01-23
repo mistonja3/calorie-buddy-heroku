c50744ba8a3e5ff071a0ce1b06f5b0f91c55d8af
mistonja3 <mario.tom96@gmail.com>
2022-01-23 11:21:06 +0900

    remove node_module folder

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ const app = express()
 const bodyParser = require("body-parser");
 const mysql = require('mysql')
 const port = process.env.PORT || 3000
-
+const dotenv = require('dotenv')
+dotenv.config({path: "./.env"})
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
 const db = mysql.createPool({
-        host: 'us-cdbr-east-05.cleardb.net',
-        user: 'b9ca21465ceee7',
-        password: '0b401c18',
-        database: 'heroku_62d38eeaddb4477'
-    })
-    // insert into food (name, typical_value, unit, calories, carbs, fat, protein, salt, sugar) VALUES ('Chocolate bar', 100, 'g', 610, 51.5, 32.7, 10.2, 0, 49.3)
-    // insert into food (name, typical_value, unit, calories, carbs, fat, protein, salt, sugar) VALUES ('Chicken breast', 100, 'g', 120, 1.5, 0.2, 20, 0, 0)
-app.use(bodyParser.urlencoded({ extended: false }));
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE
+})
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser('secret'));
 app.use(session({
     secret: 'secret',
-    cookie: { maxAge: null },
+    cookie: {
+        maxAge: null
+    },
     resave: false,
     saveUninitialized: false
 }))
-app.use(function(req, res, next) {
+
+app.use(function (req, res, next) {
     res.locals.message = req.session.message;
     delete req.session.message;
     next();
